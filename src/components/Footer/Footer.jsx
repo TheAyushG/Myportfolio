afc6b83dc54970ad840d525675293bca82ad203f
TheAyushG <jiaayush256@gmail.com>
2025-07-08 17:32:26 +0530

    Updated code with latest changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+    <footer className="text-purple-500  py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
       <div className="container mx-auto text-center">
         {/* Name / Logo */}
         <h2 className="text-xl font-semibold text-purple-500">Ayush Gurjar</h2>
@@ -28,7 +28,7 @@ const Footer = () => {
             <button
               key={index}
               onClick={() => handleScroll(item.id)}
-              className="hover:text-purple-500 text-sm sm:text-base my-1"
+              className="hover:text-white text-sm sm:text-base my-1"
             >
               {item.name}
             </button>
