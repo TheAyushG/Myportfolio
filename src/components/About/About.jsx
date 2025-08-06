e5ba62214760028dec22e5672e78ac7fcf976b9c
TheAyushG <jiaayush256@gmail.com>
2025-08-07 00:35:55 +0530

    update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -62,7 +62,7 @@ const About = () => {
         
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
-          <Tilt
+          <img
             className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
             tiltMaxAngleX={20}
             tiltMaxAngleY={20}
@@ -76,7 +76,7 @@ const About = () => {
               alt="Ayush Gurjar"
               className="w-full h-full rounded-full object-cover "
             />
-          </Tilt>
+          </img>
         </div>
       </div>
     </section>
